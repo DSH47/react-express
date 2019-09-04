33a77cfe503c0091a354b49495488edbfb5050c6
DSH47 <1461301563@qq.com>
2019-09-04 19:36:53 +0800

    add jsdata

--- pages/01page/01page.js
+++ pages/01page/01page.js
@@ -6,6 +6,84 @@ Page({
 
     currentTab: 0,
 
+    one:[{
+      id:1,
+      images:'https://upload-images.jianshu.io/upload_images/15721919-96553148cc3e6dea.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/782/format/webp',
+      two:[{
+        'id':11,
+        title:'微积分是最适合中国大多数学生顺利学出来的一门理科课。',
+        txt:'更新 一周前',
+      }]
+
+    },
+
+    {
+      id:2,
+      images:'http://ask.qcloudimg.com/http-save/yehe-1669339/mrttv45xje.jpeg',
+      two:[{
+        'id':12,
+        title:'微积分(Calculus)是高等数学中研究函数的微分、积分以及有关概念和应用的数学分支。',
+        txt:'更新 两周前',
+      }]
+    },
+
+    {
+      id: 3,
+      images: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4037085886,4011589942&fm=26&gp=0.jpg',
+      two: [{
+        'id': 13,
+        title: 'AP考试的成绩使用5分制，考生可以获得1，2，3，4或者5分,一般三分或三分以上的成绩可以在大学换取学分.',
+        txt: '更新 三周前',
+      }]
+    },
+
+    {
+      id: 4,
+      images: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4071605530,339009200&fm=11&gp=0.jpg',
+      two: [{
+        'id': 14,
+        title: 'AP微积分是由美国大学理事会（The College Board）提供的在高中授课的大学课程。',
+        txt: '更新 四周前',
+      }],
+    }],
+
+   apti:[{
+     id: 5,
+     images: 'http://img95.699pic.com/photo/50040/6494.jpg_wh300.jpg',
+     two: [{
+       'id': 15,
+       title: '微积分分为四个部分,考试时间相对比较充裕，考生只要认真备考，就可以完成题目',
+       txt: '更新 一周前',
+     }]
+   },
+   {
+     id: 6,
+     images: 'https://f10.baidu.com/it/u=2202493337,3369188385&fm=72',
+     two: [{
+       'id': 16,
+       title: '留学应该提前规划好，是充分准备后的行动，同学们应该早一点预估自己想去怎样的学校。',
+       txt: '更新 两周前',
+     }]
+   },
+   {
+     id: 7,
+     images: 'http://5b0988e595225.cdn.sohucs.com/images/20181009/d2c854a7ad1a46d2952885e222c24644.jpg',
+     two: [{
+       'id': 17,
+       title: '3月中旬开始练习真题，熟练出题形式和加深对细节知识点的掌握深度。',
+       txt: '更新 三周前',
+     }]
+   },
+   {
+     id: 8,
+     images: 'http://img95.699pic.com/photo/50021/0579.jpg_wh300.jpg',
+     two: [{
+       'id': 18,
+       title: '理解微积分的“基本思想”——“极限”至关重要，也是直接通往微分积分世界的“九阴真经”。',
+       txt: '更新 四周前',
+     }]
+   }]
+
   },
 
   swichNav: function (e) {
@@ -51,7 +129,7 @@ Page({
   },
 
   onReady: function () {
-
+  
     // 生命周期函数--监听页面初次渲染完成
 
   },
--- pages/06page/06page.js
+++ pages/06page/06page.js
@@ -5,15 +5,238 @@ Page({
    * 页面的初始数据
    */
   data: {
-    douban: [{ "rating": { "max": 10, "average": 0, "stars": "00", "min": 0 }, "genres": ["戏曲"], "title": "曹操与杨修", "casts": [{ "alt": "https://movie.douban.com/celebrity/1371573/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1520320752.41.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1520320752.41.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1520320752.41.webp" }, "name": "尚长荣", "id": "1371573" }, { "alt": "https://movie.douban.com/celebrity/1395332/", "avatars": { "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/pE2tphPQWZigcel_avatar_uploaded1528469094.77.webp", "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/pE2tphPQWZigcel_avatar_uploaded1528469094.77.webp", "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/pE2tphPQWZigcel_avatar_uploaded1528469094.77.webp" }, "name": "言兴朋", "id": "1395332" }], "collect_count": 74, "original_title": "曹操与杨修", "subtype": "movie", "directors": [{ "alt": "https://movie.douban.com/celebrity/1399708/", "avatars": { "small": "https://img1.doubanio.com/f/movie/ca527386eb8c4e325611e22dfcb04cc116d6b423/pics/movie/celebrity-default-small.png", "large": "https://img3.doubanio.com/f/movie/63acc16ca6309ef191f0378faf793d1096a3e606/pics/movie/celebrity-default-large.png", "medium": "https://img1.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png" }, "name": "滕俊杰", "id": "1399708" }], "year": "2018", "images": { "small": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2531189407.webp", "large": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2531189407.webp", "medium": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2531189407.webp" }, "alt": "https://movie.douban.com/subject/30238932/", "id": "30238932" }, { "rating": { "max": 10, "average": 8.1, "stars": "40", "min": 0 }, "genres": ["动作", "惊悚", "冒险"], "title": "碟中谍6：全面瓦解", "casts": [{ "alt": "https://movie.douban.com/celebrity/1054435/", "avatars": { "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p567.webp", "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p567.webp", "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p567.webp" }, "name": "汤姆·克鲁斯", "id": "1054435" }, { "alt": "https://movie.douban.com/celebrity/1044713/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1371934661.95.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1371934661.95.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1371934661.95.webp" }, "name": "亨利·卡维尔", "id": "1044713" }, { "alt": "https://movie.douban.com/celebrity/1048129/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p8712.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p8712.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p8712.webp" }, "name": "文·瑞姆斯", "id": "1048129" }], "collect_count": 30536, "original_title": "Mission: Impossible - Fallout", "subtype": "movie", "directors": [{ "alt": "https://movie.douban.com/celebrity/1276314/", "avatars": { "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p52867.webp", "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p52867.webp", "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p52867.webp" }, "name": "克里斯托弗·麦奎里", "id": "1276314" }], "year": "2018", "images": { "small": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529365085.webp", "large": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529365085.webp", "medium": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529365085.webp" }, "alt": "https://movie.douban.com/subject/26336252/", "id": "26336252" }, { "rating": { "max": 10, "average": 7.4, "stars": "40", "min": 0 }, "genres": ["剧情", "运动"], "title": "苏丹", "casts": [{ "alt": "https://movie.douban.com/celebrity/1017831/", "avatars": { "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355291691.29.webp", "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355291691.29.webp", "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355291691.29.webp" }, "name": "萨尔曼·汗", "id": "1017831" }, { "alt": "https://movie.douban.com/celebrity/1045145/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1431791068.5.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1431791068.5.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1431791068.5.webp" }, "name": "安努舒卡·莎玛", "id": "1045145" }, { "alt": "https://movie.douban.com/celebrity/1338529/", "avatars": { "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1423050954.69.webp", "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1423050954.69.webp", "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1423050954.69.webp" }, "name": "马雷塞·克伦普", "id": "1338529" }], "collect_count": 2195, "original_title": "Sultan", "subtype": "movie", "directors": [{ "alt": "https://movie.douban.com/celebrity/1378742/", "avatars": { "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1502246478.77.webp", "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1502246478.77.webp", "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1502246478.77.webp" }, "name": "阿里·阿巴斯·札法", "id": "1378742" }], "year": "2016", "images": { "small": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529570494.webp", "large": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529570494.webp", "medium": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529570494.webp" }, "alt": "https://movie.douban.com/subject/26728641/", "id": "26728641" }, { "rating": { "max": 10, "average": 0, "stars": "00", "min": 0 }, "genres": ["纪录片"], "title": "藏北秘岭-重返无人区", "casts": [{ "alt": "https://movie.douban.com/celebrity/1396890/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124303.2.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124303.2.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124303.2.webp" }, "name": "蔡宇", "id": "1396890" }, { "alt": "https://movie.douban.com/celebrity/1396889/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp" }, "name": "饶子君", "id": "1396889" }, { "alt": "https://movie.douban.com/celebrity/1396891/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124315.6.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124315.6.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124315.6.webp" }, "name": "土旦巴桑", "id": "1396891" }], "collect_count": 69, "original_title": "藏北秘岭-重返无人区", "subtype": "movie", "directors": [{ "alt": "https://movie.douban.com/celebrity/1396889/", "avatars": { "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp", "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp", "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp" }, "name": "饶子君", "id": "1396889" }], "year": "2018", "images": { "small": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2530299035.webp", "large": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2530299035.webp", "medium": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2530299035.webp" }, "alt": "https://movie.douban.com/subject/30208007/", "id": "30208007" }]
-   
+    douban: [{
+      "rating": {
+        "max": 10,
+        "average": 0,
+        "stars": "00",
+        "min": 0
+      },
+      "genres": ["戏曲"],
+      "title": "曹操与杨修",
+      "casts": [{
+        "alt": "https://movie.douban.com/celebrity/1371573/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1520320752.41.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1520320752.41.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1520320752.41.webp"
+        },
+        "name": "尚长荣",
+        "id": "1371573"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1395332/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/pE2tphPQWZigcel_avatar_uploaded1528469094.77.webp",
+          "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/pE2tphPQWZigcel_avatar_uploaded1528469094.77.webp",
+          "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/pE2tphPQWZigcel_avatar_uploaded1528469094.77.webp"
+        },
+        "name": "言兴朋",
+        "id": "1395332"
+      }],
+      "collect_count": 74,
+      "original_title": "曹操与杨修",
+      "subtype": "movie",
+      "directors": [{
+        "alt": "https://movie.douban.com/celebrity/1399708/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/f/movie/ca527386eb8c4e325611e22dfcb04cc116d6b423/pics/movie/celebrity-default-small.png",
+          "large": "https://img3.doubanio.com/f/movie/63acc16ca6309ef191f0378faf793d1096a3e606/pics/movie/celebrity-default-large.png",
+          "medium": "https://img1.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png"
+        },
+        "name": "滕俊杰",
+        "id": "1399708"
+      }],
+      "year": "2018",
+      "images": {
+        "small": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2531189407.webp",
+        "large": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2531189407.webp",
+        "medium": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2531189407.webp"
+      },
+      "alt": "https://movie.douban.com/subject/30238932/",
+      "id": "30238932"
+    }, {
+      "rating": {
+        "max": 10,
+        "average": 8.1,
+        "stars": "40",
+        "min": 0
+      },
+      "genres": ["动作", "惊悚", "冒险"],
+      "title": "碟中谍6：全面瓦解",
+      "casts": [{
+        "alt": "https://movie.douban.com/celebrity/1054435/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p567.webp",
+          "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p567.webp",
+          "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p567.webp"
+        },
+        "name": "汤姆·克鲁斯",
+        "id": "1054435"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1044713/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1371934661.95.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1371934661.95.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1371934661.95.webp"
+        },
+        "name": "亨利·卡维尔",
+        "id": "1044713"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1048129/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p8712.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p8712.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p8712.webp"
+        },
+        "name": "文·瑞姆斯",
+        "id": "1048129"
+      }],
+      "collect_count": 30536,
+      "original_title": "Mission: Impossible - Fallout",
+      "subtype": "movie",
+      "directors": [{
+        "alt": "https://movie.douban.com/celebrity/1276314/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p52867.webp",
+          "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p52867.webp",
+          "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p52867.webp"
+        },
+        "name": "克里斯托弗·麦奎里",
+        "id": "1276314"
+      }],
+      "year": "2018",
+      "images": {
+        "small": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529365085.webp",
+        "large": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529365085.webp",
+        "medium": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529365085.webp"
+      },
+      "alt": "https://movie.douban.com/subject/26336252/",
+      "id": "26336252"
+    }, {
+      "rating": {
+        "max": 10,
+        "average": 7.4,
+        "stars": "40",
+        "min": 0
+      },
+      "genres": ["剧情", "运动"],
+      "title": "苏丹",
+      "casts": [{
+        "alt": "https://movie.douban.com/celebrity/1017831/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355291691.29.webp",
+          "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355291691.29.webp",
+          "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355291691.29.webp"
+        },
+        "name": "萨尔曼·汗",
+        "id": "1017831"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1045145/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1431791068.5.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1431791068.5.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1431791068.5.webp"
+        },
+        "name": "安努舒卡·莎玛",
+        "id": "1045145"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1338529/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1423050954.69.webp",
+          "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1423050954.69.webp",
+          "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1423050954.69.webp"
+        },
+        "name": "马雷塞·克伦普",
+        "id": "1338529"
+      }],
+      "collect_count": 2195,
+      "original_title": "Sultan",
+      "subtype": "movie",
+      "directors": [{
+        "alt": "https://movie.douban.com/celebrity/1378742/",
+        "avatars": {
+          "small": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1502246478.77.webp",
+          "large": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1502246478.77.webp",
+          "medium": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1502246478.77.webp"
+        },
+        "name": "阿里·阿巴斯·札法",
+        "id": "1378742"
+      }],
+      "year": "2016",
+      "images": {
+        "small": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529570494.webp",
+        "large": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529570494.webp",
+        "medium": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2529570494.webp"
+      },
+      "alt": "https://movie.douban.com/subject/26728641/",
+      "id": "26728641"
+    }, {
+      "rating": {
+        "max": 10,
+        "average": 0,
+        "stars": "00",
+        "min": 0
+      },
+      "genres": ["纪录片"],
+      "title": "藏北秘岭-重返无人区",
+      "casts": [{
+        "alt": "https://movie.douban.com/celebrity/1396890/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124303.2.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124303.2.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124303.2.webp"
+        },
+        "name": "蔡宇",
+        "id": "1396890"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1396889/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp"
+        },
+        "name": "饶子君",
+        "id": "1396889"
+      }, {
+        "alt": "https://movie.douban.com/celebrity/1396891/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124315.6.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124315.6.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124315.6.webp"
+        },
+        "name": "土旦巴桑",
+        "id": "1396891"
+      }],
+      "collect_count": 69,
+      "original_title": "藏北秘岭-重返无人区",
+      "subtype": "movie",
+      "directors": [{
+        "alt": "https://movie.douban.com/celebrity/1396889/",
+        "avatars": {
+          "small": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp",
+          "large": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp",
+          "medium": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1531124289.64.webp"
+        },
+        "name": "饶子君",
+        "id": "1396889"
+      }],
+      "year": "2018",
+      "images": {
+        "small": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2530299035.webp",
+        "large": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2530299035.webp",
+        "medium": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2530299035.webp"
+      },
+      "alt": "https://movie.douban.com/subject/30208007/",
+      "id": "30208007"
+    }]
+
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   }
 })
